d001991f1584386ec781e544bda543ea0f2d952c
Aaron Moore <aaron.mv.moore@gmail.com>
2023-01-26 14:45:15 -0600

    Completed the exercises from the LIMIT lesson

---
+++ limit_exercises.sql
@@ -0,0 +1,31 @@
+USE employees;
+
+-- List the first 10 distinct last name sorted in descending order.
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+-- Find all previous or current employees hired in the 90s and born on Christmas
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%'
+	AND birth_date LIKE '%12-25';
+	
+-- Find the first 5 employees hired in the 90's by sorting by hire date and limiting your results to the first 5 records.
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%'
+	AND birth_date LIKE '%12-25'
+ORDER BY hire_date
+LIMIT 5;
+
+-- Update the query to find the tenth page (1 page = 5 results) of results.
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%'
+	AND birth_date LIKE '%12-25'
+ORDER BY hire_date
+LIMIT 5 OFFSET 50;
+
+-- OFFSET allows us to skip tp the page number we would like to begin on. The LIMIT will begin its count on the item directly after the OFFSET.
