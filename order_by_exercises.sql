1171d1e664c101e2e20bc9b2921d03ec8c39d129
Aaron Moore <aaron.mv.moore@gmail.com>
2023-01-26 15:05:07 -0600

    Corrected mistakes found after a self-check

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -11,38 +11,53 @@ FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 ORDER BY `first_name`;
 
--- 3
--- A: Irena Acton, Vidya Zweizig
+-- 3 Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by first name and then last name. In your comments, answer: 
+-- What was the first and last name in the first row of the results? 
+	-- A: Irena Acton
+-- What was the first and last name of the last person in the table?
+	-- A: Vidya Zweizig
 SELECT *
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 ORDER BY `first_name`, last_name;
 
--- 4
-	-- A: Irena Acton, Maya Zyda
+-- 4 Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by last name and then first name. In your comments, answer: 
+-- What was the first and last name in the first row of the results?
+	-- A:  Irena Acton
+-- What was the first and last name of the last person in the table?
+	-- A: Maya Zyda
 SELECT *
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 ORDER BY last_name, first_name;
 
--- 5
-	-- A: 899. 10021	Ramzi	Erde; 499648	Tadahiro	Erde
+-- 5 Write a query to to find all employees whose last name starts and ends with 'E'. Sort the results by their employee number. 
+	-- the number of employees returned
+	-- the first employee number and their first and last name,
+	-- last employee number with their first and last name.
+		-- A: 899; 10021 Ramzi Erde; 499648	Tadahiro	Erde
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%e' 
 ORDER BY emp_no;
 
--- 6
-	-- A: 899, NEWEST: 67892	Teiji	Eldridge	1999-11-27; OLDEST: 233488	Sergi	Erde	1985-02-02
+-- 6 Sort the results by their hire date, so that the newest employees are listed first.
+	-- the number of employees returned
+	-- the name of the newest employee
+	-- the name of the oldest employee.
+		-- A: 899; NEWEST: Teiji Eldridge; OLDEST: Sergi Erde
 SELECT emp_no, first_name, last_name, hire_date
 FROM employees
 WHERE last_name LIKE 'E%e' 
 ORDER BY hire_date DESC;
 
--- 7
-	-- A: 33936	1952-12-25	Khun	Bernini	M	1999-08-31
-SELECT *
+-- 7 Find all employees hired in the 90s and born on Christmas. Sort the results so that the oldest employee who was hired last is the first result. 
+	-- the number of employees returned
+	-- the name of the oldest employee who was hired last
+	-- the name of the youngest employee who was hired first.
+		-- A: 33936; Oldest Last Hire: Gudjon Vakili; Youngest Early Hire: Tremaine Eugenio
+SELECT first_name, last_name
 FROM employees
 WHERE birth_date LIKE '%12-25'
 AND hire_date LIKE '199%'
-ORDER BY hire_date DESC, birth_date DESC;
+ORDER BY birth_date DESC, hire_date DESC;
