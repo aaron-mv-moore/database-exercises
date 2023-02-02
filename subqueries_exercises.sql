c9aae3e84c06a2387b16e98313a5b8d3dacfb164
Aaron Moore <aaron.mv.moore@gmail.com>
2023-02-02 12:00:06 -0600

    Completed required exercises

---
+++ subqueries_exercises.sql
@@ -0,0 +1,109 @@
+-- Subqueries Exercises
+USE employees;
+SELECT * FROM employees LIMIT 10;
+
+-- 1. Find all the current employees with the same hire date as employee 101010 using a subquery.
+SELECT *
+FROM employees
+WHERE hire_date = 
+	(SELECT `hire_date`
+	FROM employees
+	WHERE emp_no = 101010
+);
+
+-- 2. Find all the titles ever held by all current employees with the first name Aamod.
+
+SELECT first_name, title
+FROM (SELECT employees.emp_no, first_name
+	FROM employees JOIN dept_emp
+		USING (emp_no)
+	WHERE first_name = 'AAMOD'
+		AND to_date > now()) as aamod
+JOIN titles
+	USING (emp_no);
+	
+-- 3. How many people in the employees table are no longer working for the company? Give the answer in a comment in your code.
+	-- 85108
+
+SELECT COUNT(*)
+FROM (
+	SELECT DISTINCT employees.emp_no
+	FROM employees
+	JOIN dept_emp
+		USING (emp_no)
+	WHERE to_date < now()) as nh;
+
+-- 4. Find all the current department managers that are female. List their names in a comment in your code.
+	 -- Isamu, Karsten, Leon, Hilary
+
+SELECT *
+FROM (
+	SELECT first_name, last_name, gender, to_date
+	FROM employees JOIN dept_manager
+		USING (emp_no)
+		WHERE gender = 'F'
+		) as fm
+ WHERE to_date > now();
+ 
+-- 5. Find all the employees who currently have a higher salary than the companies overall, historical average salary.
+
+SELECT emp_no, first_name, last_name, salary, (SELECT avg(salary)
+	FROM salaries)
+FROM employees
+JOIN salaries
+	USING (emp_no)
+WHERE salary > 
+	(SELECT avg(salary)
+	FROM salaries)
+	AND salaries.to_date > now();
+	
+-- 6. How many current salaries are within 1 standard deviation of the current highest salary? 220
+SELECT STDDEV(salary)
+FROM salaries 
+WHERE to_date > now();
+
+SELECT count(*)
+FROM salaries
+WHERE salary BETWEEN (
+			(SELECT max(salary) 
+				FROM salaries 
+				WHERE to_date > now()
+				) - 
+				(SELECT STDDEV(salary) 
+				FROM salaries 
+				WHERE to_date > now())
+				) 
+				AND (
+				(SELECT max(salary) 
+				FROM salaries 
+				WHERE to_date > now()
+				) + 
+				(SELECT STDDEV(salary) 
+				FROM salaries 
+				WHERE to_date > now()
+				));
+
+--  What percentage of all salaries is this? 0.0077%
+SELECT (count(*) / 
+				(SELECT COUNT(*) 
+					FROM salaries)
+					) * 100
+FROM salaries
+WHERE salary BETWEEN (
+			(SELECT max(salary) 
+				FROM salaries 
+				WHERE to_date > now()
+				) - 
+				(SELECT STDDEV(salary) 
+				FROM salaries 
+				WHERE to_date > now())
+				) 
+				AND (
+				(SELECT max(salary) 
+				FROM salaries 
+				WHERE to_date > now()
+				) + 
+				(SELECT STDDEV(salary) 
+				FROM salaries 
+				WHERE to_date > now()
+				));
