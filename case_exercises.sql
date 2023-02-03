7ee13229fc18ad16c14cea03fea3c91cf0008b08
Aaron Moore <aaron.mv.moore@gmail.com>
2023-02-03 12:11:24 -0600

    Corrected mistakes and updated format for readability

--- case_exercises.sql
+++ case_exercises.sql
@@ -1,23 +1,22 @@
 -- CASE Exercises --
 USE employees;
--- 1. Write a query that returns all employees, their department number, their start date, their end date, and a new column 'is_current_employee' that is a 1 if the employee is still with the company and 0 if not.
 
--- what is this asking? Do we want the most resent position along with if they are a part of the company?
+-- 1. Write a query that returns all employees, their department number, their start date, their end date, and a new column 'is_current_employee' that is a 1 if the employee is still with the company and 0 if not.
 DESC employees;
 DESC dept_emp;
 
-SELECT CONCAT(last_name, ', ', first_name) AS emp_name,
+SELECT 
+	CONCAT(last_name, ', ', first_name) AS emp_name,
 	dept_no, from_date, to_date,
 	IF(to_date > now(), TRUE, FALSE) AS is_current_employee
 FROM dept_emp
-JOIN employees
-	USING (emp_no)
-LIMIT 10
-;
+	JOIN employees
+		USING (emp_no);
 
 -- 2. Write a query that returns all employee names (previous and current), and a new column 'alpha_group' that returns 'A-H', 'I-Q', or 'R-Z' depending on the first letter of their last name.
 
-SELECT CONCAT(last_name, ', ', first_name) AS emp_name,
+SELECT 
+	CONCAT(last_name, ', ', first_name) AS emp_name,
 	CASE
 		WHEN last_name LIKE 'A%' THEN 'A-H'
 		WHEN last_name LIKE 'B%' THEN 'A-H'
@@ -42,21 +41,25 @@ FROM employees;
 
 -- 3. How many employees (current or previous) were born in each decade?
 DESC employees;
+
 SELECT min(birth_date), max(birth_date)
-FROM employees;
+FROM employees; -- Using this query to understand which decades need to be included in the next query
+
 SELECT 
 	CASE 
 		WHEN YEAR(birth_date) LIKE '__6_' THEN '1960'
 		WHEN YEAR(birth_date) LIKE '__5_' THEN '1950' 
 	END AS birth_decade,
 	COUNT(*)
-FROM (SELECT DISTINCT emp_no, birth_date FROM employees) AS sans_duplicates
+FROM employees
 GROUP BY birth_decade;
 
 -- 4. What is the current average salary for each of the following department groups: R&D, Sales & Marketing, Prod & QM, Finance & HR, Customer Service?
 
 -- current salaries
-SELECT emp_no, salary 
+SELECT 
+	emp_no, 
+	salary 
 FROM salaries
 WHERE to_date > now();
 
@@ -65,44 +68,48 @@ SELECT dept_name
 FROM departments;
 
 -- combine the two and get average salary for each departments
-SELECT dept_name, avg(salary)
+SELECT 
+	dept_name, 
+	avg(salary)
 FROM departments
-JOIN dept_emp
-	USING (dept_no)
-JOIN (SELECT emp_no, salary 
-		FROM salaries
-		WHERE to_date > now()) AS s
-	USING (emp_no)
+	JOIN dept_emp AS de
+		USING (dept_no)
+	JOIN salaries AS s
+		USING (emp_no)
+WHERE s.to_date > now()
+	AND de.to_date > now()
 GROUP BY dept_name;
 
 -- Creating a new column with consolidated names of departments
 
 SELECT 
-CASE 
-	WHEN dept_name IN ('research', 'development') THEN 'R&D'
-	WHEN dept_name IN ('sales', 'marketing') THEN 'Sales & Marketing'
-	WHEN dept_name IN ('production', 'quality management') THEN 'Prod & QM'
-	WHEN dept_name IN ('finance', 'human resources') THEN 'Finance & HR'
-	ELSE 'Customer Service'
-END AS dept_groups,
-count(*)
+	CASE 
+		WHEN dept_name IN ('research', 'development') THEN 'R&D'
+		WHEN dept_name IN ('sales', 'marketing') THEN 'Sales & Marketing'
+		WHEN dept_name IN ('production', 'quality management') THEN 'Prod & QM'
+		WHEN dept_name IN ('finance', 'human resources') THEN 'Finance & HR'
+		ELSE 'Customer Service'
+	END AS dept_groups,
+	COUNT(*)
 FROM departments
 GROUP BY dept_groups;
 
 
 -- Putting it all together
-SELECT CASE 
-	WHEN dept_name IN ('research', 'development') THEN 'R&D'
-	WHEN dept_name IN ('sales', 'marketing') THEN 'Sales & Marketing'
-	WHEN dept_name IN ('production', 'quality management') THEN 'Prod & QM'
-	WHEN dept_name IN ('finance', 'human resources') THEN 'Finance & HR'
-	ELSE 'Customer Service'
-END AS dept_group, avg(salary)
+SELECT 
+	CASE 
+		WHEN dept_name IN ('research', 'development') THEN 'R&D'
+		WHEN dept_name IN ('sales', 'marketing') THEN 'Sales & Marketing'
+		WHEN dept_name IN ('production', 'quality management') THEN 'Prod & QM'
+		WHEN dept_name IN ('finance', 'human resources') THEN 'Finance & HR'
+		ELSE 'Customer Service'
+	END AS dept_group,
+	AVG(salary)
 FROM departments
-JOIN dept_emp
-	USING (dept_no)
-JOIN (SELECT emp_no, salary 
-		FROM salaries
-		WHERE to_date > now()) AS s
-	USING (emp_no)
+	JOIN dept_emp AS de
+		USING (dept_no)
+	JOIN salaries AS s
+		USING (emp_no)
+WHERE s.to_date > now()
+	AND de.to_date > now()
 GROUP BY dept_group;
