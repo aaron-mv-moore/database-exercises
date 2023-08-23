ad40826e6bdec21c06810d22813b39248d801195
Aaron Moore <122934810+aaron-mv-moore@users.noreply.github.com>
2023-08-23 17:21:42 -0400

    Update hacker_rank_basic.sql

--- hacker_rank_basic.sql
+++ hacker_rank_basic.sql
@@ -266,6 +266,78 @@ SELECT CEIL(AVG(salary) - AVG(REPLACE(salary, '0', '')))
 FROM employees;
 
 
+/*
+Top Earners
+We define an employee's total earnings to be their monthly salary x months worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as  space-separated integers.
+*/
+SELECT
+  (salary * months), count(employee_id)
+FROM employee
+GROUP BY (salary * months)
+ORDER BY (salary * months) DESC
+LIMIT 1
+;
+
+/*
+Weather Observation Station 2
+Query the following two values from the STATION table:
+The sum of all values in LAT_N rounded to a scale of 2 decimal places.
+The sum of all values in LONG_W rounded to a scale of 2 decimal places.
+*/
+SELECT
+  ROUND(SUM(lat_n), 2),
+  ROUND(SUM(long_w), 2)
+FROM station;
+
+/*
+Weather Observation Station 13
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. Truncate your answer to 4 decimal places.
+*/
+SELECT
+  TRUNCATE(SUM(lat_n), 4)
+FROM station
+WHERE 
+  lat_n BETWEEN 38.7880 AND 137.2345;
+
+/*
+Weather Observation Station 14
+Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345 Truncate your answer to 4 decimal places.
+*/
+SELECT
+  TRUNCATE(MAX(lat_n), 4)
+FROM station
+WHERE lat_n < 137.2345;
+
+/*
+Weather Observation Station 15
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. Round your answer to 4 decimal places.
+*/
+SELECT
+  ROUND(long_w, 4)
+FROM station
+WHERE lat_n < 137.2345
+ORDER BY lat_n DESC
+LIMIT 1;
+
+/*
+Weather Observation Station 16
+Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780. Round your answer to 4 decimal places.
+*/
+SELECT
+  ROUND(MIN(lat_n), 4)
+FROM station
+WHERE lat_n > 38.7880;
+
+/*
+Weather Observation Station 17
+Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than . Round your answer to  decimal places.
+*/
+SELECT 
+  ROUND(long_w, 4)
+FROM station
+WHERE lat_n  > 38.7880
+ORDER BY lat_n
+LIMIT 1; 
 
 
 
